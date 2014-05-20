50fb9efee7714943f73a3d1875f01c42bba53010
Oleg Slobodskoi <oleg008@gmail.com>
2014-05-20 02:23:44 +0200

    basic layout wip

--- app/src/main.js
+++ app/src/main.js
@@ -5,25 +5,60 @@ define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var Scrollview = require("famous/views/Scrollview");
+    var View = require('famous/core/View');
+    var ViewSequence = require('famous/core/ViewSequence');
+    var Surface = require('famous/core/Surface');
+    var EventHandler = require('famous/core/EventHandler');
+    var OptionsManager = require('famous/core/OptionsManager');
+    var FlexibleLayout = require('famous/views/FlexibleLayout');
 
-    // create the main context
-    var mainContext = Engine.createContext();
+    var mainContext = Engine.createContext()
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
-    });
+    function App() {
+        View.apply(this, arguments)
+        this.header = new Header()
+        this.content = new Content()
+        this.navigation = new View()
+        this.scrollview = new Scrollview()
+        this.add(this.scrollview)
+        var views = [this.header, this.content]
+        this.scrollview.sequenceFrom(views)
+        views.forEach(function(view) {
+            view._eventInput.pipe(this.scrollview)
+        }, this)
+    }
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
+    App.prototype = Object.create(View.prototype)
 
-    mainContext.add(centerSpinModifier).add(logo);
-});
+    function Header() {
+        View.apply(this, arguments)
+        this.surface = new Surface({
+            content: 'Header',
+            size: [undefined, 200],
+            properties: {
+                backgroundColor: 'red'
+            }
+        })
+        this.add(this.surface)
+        this.surface.pipe(this)
+    }
+
+    Header.prototype = Object.create(View.prototype)
+
+    function Content() {
+        View.apply(this, arguments)
+        this.surface = new Surface({
+            content: 'Content',
+            properties: {
+                backgroundColor: 'green'
+            }
+        })
+        this.add(this.surface)
+        this.surface.pipe(this)
+    }
+
+    Content.prototype = Object.create(View.prototype)
+
+    mainContext.add(new App());
+})
