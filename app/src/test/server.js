bf462d68714ebec1451515cae9ae45e4cdc4dba5
Oleg Slobodskoi <oleg008@gmail.com>
2014-05-26 16:39:06 +0200

    test/server

---
+++ app/src/test/server.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+    'use strict'
+    require('sinon')
+    // XXX needs better packaging.
+    var sinon = window.sinon
+    var server = module.exports = sinon.fakeServer.create()
+    server.autoRespond = true
+    //server.autoRespondAfter = 500;
+})
