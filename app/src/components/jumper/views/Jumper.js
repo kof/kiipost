713ff47852a9368ade674afc846e94687c167b6a
Oleg Slobodskoi <oleg008@gmail.com>
2014-05-28 22:08:57 +0200

    jumper animation

--- app/src/components/jumper/views/Jumper.js
+++ app/src/components/jumper/views/Jumper.js
@@ -2,20 +2,29 @@ define(function(require, exports, module) {
     'use strict'
 
     var inherits = require('inherits')
+    var _ = require('underscore')
 
     var View = require('famous/core/View')
     var Surface = require('famous/core/Surface')
+    var Transform = require('famous/core/Transform')
+    var StateModifier = require('famous/modifiers/StateModifier')
+
+    var app = require('app')
 
     function Jumper() {
         View.apply(this, arguments)
         this.scrollview = this.options.scrollview
         this.hidden = true
+
         this.surface = new Surface({
-            size: [undefined, 60],
-            classes: ['jumper'],
-            content: '<span class="icomatic">arrowup</span>'
+            content: 'arrowup',
+            size: this.options.size,
+            classes: ['jumper', 'icomatic']
         })
-        this.add(this.surface)
+        this.modifier = new StateModifier({origin: [0.5, 0.05]})
+        this.modifier.setTransform(Transform.scale(0, 0))
+        this.add(this.modifier).add(this.surface)
+
         this.surface.on('click', this._onClick.bind(this))
         this.scrollview.sync.on('update', this._onScroll.bind(this))
     }
@@ -24,23 +33,29 @@ define(function(require, exports, module) {
     module.exports = Jumper
 
     Jumper.DEFAULT_OPTIONS = {
+        size: [45, 45],
         scrollview: null,
         // Min amount of px to scroll back before jumper will be shown.
-        scrollBackDelta: 3
+        scrollBackDelta: 3,
+        duration: 200
     }
 
-    Jumper.prototype.hide = function() {
-        this.surface.setProperties({display: 'none'})
+    Jumper.prototype.hide = _.debounce(function() {
+        this.modifier.setTransform(Transform.scale(0, 0), {duration: this.options.duration})
         this.hidden = true
-    }
+    }, 500, true)
+
+    Jumper.prototype.show = _.debounce(function() {
+        this.modifier.setTransform(Transform.scale(1, 1), {duration: this.options.duration})
+        this.hidden = false
+    }, 500, true)
 
     Jumper.prototype._onScroll = function(e) {
         if (e.delta >= this.options.scrollBackDelta) {
             // XXX After scrolling down and up, pageSpringPosition value never
             // gets its original value 0
             if (this.scrollview._pageSpringPosition < -3) {
-                this.surface.setProperties({display: 'block'})
-                this.hidden = false
+                this.show()
             } else {
                 this.hide()
             }
