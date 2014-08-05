0d06a9a2c272ac5e8d0a9f852e375e3447d63539
Oleg Slobodskoi <oleg008@gmail.com>
2014-08-05 13:28:19 +0200

    #116 no default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,17 @@ var program = require('commander')
 
 program
     .option('-c, --cordova', 'build for cordova', Boolean)
+    // TODO add all commands for documentation purpose.
     .parse(process.argv)
+
+if (!program.args.length) return program.help()
 
 var task = {}
 ;['clean', 'css', 'copy', 'js', 'html', 'test', 'lint', 'ln', 'cordova', 'api'].forEach(function(name) {
     task[name] = require('./gulp/' + name)
 })
 
-var cordova = program.cordova || process.argv[2] == 'cordova'
+var cordova = program.cordova || program.args[0] == 'cordova'
 var env = process.env.ENV
 var app = './node_modules/app'
 var dest = './dist/' + env
@@ -26,14 +29,17 @@ gulp.task('clean', task.clean({
     dest: dest + '/**'
 }))
 
-gulp.task('css', task.css({
-    src: app + '/index.css',
-    dest: dest
+gulp.task('html', task.html({
+    src: app + '/index.html',
+    dest: dest,
+    env: env,
+    cordova: cordova
 }))
 
-gulp.task('content', task.copy({
-    src: app + '/**/*.{png,jpg,svg,eot,ttf,woff}',
-    dest: dest
+gulp.task('css', task.css({
+    src: app + '/index.css',
+    dest: dest,
+    env: env
 }))
 
 gulp.task('js', task.js({
@@ -42,11 +48,9 @@ gulp.task('js', task.js({
     env: env
 }))
 
-gulp.task('html', task.html({
-    src: app + '/index.html',
-    dest: dest,
-    env: env,
-    cordova: cordova
+gulp.task('content', task.copy({
+    src: app + '/**/*.{png,jpg,svg,eot,ttf,woff}',
+    dest: dest
 }))
 
 gulp.task('api', task.api())
@@ -56,12 +60,6 @@ gulp.task('watch-app', ['build'], function() {
     gulp.watch(app + '/**/*.html', ['html'])
     gulp.watch(app + '/**/*.css', ['css'])
     gulp.watch(app + '/**/*.{png,jpg,svg,eot,ttf,woff}', ['content'])
-})
-
-gulp.task('start', ['watch-app', 'api'])
-
-gulp.task('build', function(callback) {
-    sequence('clean', ['css', 'content', 'js', 'html'], callback)
 })
 
 gulp.task('test', task.test())
@@ -75,6 +73,10 @@ gulp.task('ln', task.ln({
     dest: './node_modules'
 }))
 
-gulp.task('cordova', ['build'], task.cordova())
+gulp.task('start', ['watch-app', 'api'])
 
-gulp.task('default', ['build'])
+gulp.task('build', function(callback) {
+    sequence('clean', ['css', 'content', 'js', 'html'], callback)
+})
+
+gulp.task('cordova', ['build'], task.cordova())
