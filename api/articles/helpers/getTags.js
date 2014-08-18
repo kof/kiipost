c0b7368403074782b9fca811da0c56d3e3bb6173
Oleg Slobodskoi <oleg008@gmail.com>
2014-08-18 23:49:39 +0200

    #147 increased relevancy by modeling dna

--- api/articles/helpers/getTags.js
+++ api/articles/helpers/getTags.js
@@ -2,7 +2,13 @@ var _ = require('underscore')
 var m = require('mongoose')
 
 // Amount of tags we take from each saved article to find new articles.
-var TAGS_AMOUNT = 3
+var SEARCH_TAGS_AMOUNT = 3
+
+// Amount of tags considered for calculating the dna.
+var DNA_TAGS_AMOUNT = 5
+
+// Min fequency tag should be used across different memos, to get into the dna.
+var DNA_MIN_FREQ = 3
 
 /**
  * Get user tags for articles lookup.
@@ -13,26 +19,50 @@ var TAGS_AMOUNT = 3
  */
 module.exports = function(userId, full) {
     return function* () {
-        var memos
-
-        memos = yield m.model('memo')
+        var memos = yield m.model('memo')
             .find({userId: userId})
             .select(full ? {} : {'articles.tags': 1})
             .lean()
             .exec()
 
-        var map = {}, data = []
 
-        memos.forEach(function(memo) {
+        // Create a tags frequency map.
+        var tagsFreqMap = {}
+        _(memos).each(function(memo) {
             var article = memo.articles[0]
-            if (!article || article.tags.length < TAGS_AMOUNT) return
-            var tags = _(article.tags).first(TAGS_AMOUNT).sort()
+            if (!article || !article.tags.length) return
+            var tags = _(article.tags).first(DNA_TAGS_AMOUNT)
+            _(tags).each(function(tag) {
+                if (tagsFreqMap[tag]) tagsFreqMap[tag]++
+                else tagsFreqMap[tag] = 1
+            })
+        })
+        // Create a tags dna array.
+        var dna = []
+        _(tagsFreqMap).each(function(freq, tag) {
+            if (freq >= DNA_MIN_FREQ) dna.push(tag)
+        })
+
+        var map = {}
+        var data = []
+
+        _(memos).each(function(memo) {
+            var article = memo.articles[0]
+
+            // Filter memos where tags amount is too low.
+            if (!article || article.tags.length < SEARCH_TAGS_AMOUNT) return
+
+            // Verify if this tags fit our dna, don't use them if not
+            var maybeDna = _(article.tags).first(DNA_TAGS_AMOUNT)
+            if (!_.intersection(maybeDna, dna).length) return
+
+            var searchTags = _(article.tags).first(SEARCH_TAGS_AMOUNT).sort()
             // Create map to avoid duplicates
-            var tagsStr = tags.toString()
+            var tagsStr = String(searchTags)
             if (!map[tagsStr]) {
                 map[tagsStr] = true
                 data.push({
-                    tags: tags,
+                    tags: searchTags,
                     memo: memo
                 })
             }
