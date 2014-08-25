b7fe5d8e2c629f5ae89895fbc1403282b08124c3
Oleg Slobodskoi <oleg008@gmail.com>
2014-08-25 14:00:00 +0200

    new sentry dev key

--- api/conf/dev.js
+++ api/conf/dev.js
@@ -1,5 +1,5 @@
 // For the clients.
 exports.client = {
     baseUrl: 'http://kiipost-dev.herokuapp.com',
-    sentryDsn: 'https://16045f69f28a46aea86e6dc7ac253aa5@app.getsentry.com/26904'
+    sentryDsn: 'https://fac0789047e74ae2ba26e7a7cf10e737@app.getsentry.com/29260'
 }
